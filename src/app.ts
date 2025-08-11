50c8f5c9e35ff486da4a99a64eaae9405cdc2e7b
Leonardo Freire Russi <leo.freirerussi@icloud.com>
2025-08-11 08:02:15 -0300

    Fundamentos do Prisma ORM

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
 import fastify from 'fastify'
+import { PrismaClient } from 'generated/prisma'
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'Leonardo Freire',
+    email: 'leonardo@freire.com',
+  },
+})
