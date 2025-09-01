6f23301882e5d7e3faa50e43c755bf60a7d184c8
Leonardo Freire Russi <freirerussi@gmail.com>
2025-09-01 00:21:50 -0300

    01_setembro_2025

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,24 @@
 import fastify from 'fastify'
-import { PrismaClient } from 'generated/prisma'
+import { appRoutes } from './http/routes'
+import z, { ZodError } from 'zod'
+import { env } from './env'
 
 export const app = fastify()
 
-const prisma = new PrismaClient()
+app.register(appRoutes)
 
-prisma.user.create({
-  data: {
-    name: 'Leonardo Freire',
-    email: 'leonardo@freire.com',
-  },
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: 'Validation error', issues: z.treeifyError(error) })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  } else {
+    // external log tool
+  }
+
+  return reply.status(500).send({ message: 'Internal server error.' })
 })
