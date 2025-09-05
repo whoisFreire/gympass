4f16f989d96deae5eeba461eb725fb80e829e163
Leonardo Freire Russi <freirerussi@gmail.com>
2025-09-05 19:43:00 -0300

    Controller de criação de academia

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -1,8 +1,8 @@
 import { FastifyInstance } from 'fastify'
-import { register } from './controllers/register'
-import { authenticate } from './controllers/authenticate'
-import { profile } from './controllers/profile'
-import { verifyJWT } from './middlewares/verify-jwt'
+import { register } from './register'
+import { authenticate } from './authenticate'
+import { profile } from './profile'
+import { verifyJWT } from '../../middlewares/verify-jwt'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
