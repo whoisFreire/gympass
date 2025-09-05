e390d335517b414069a70eb9fae187dc5d0d9d2f
Leonardo Freire Russi <freirerussi@gmail.com>
2025-09-05 19:22:35 -0300

    Teste E2E do registro

---
+++ prisma/vite-environment-prisma/prisma-test-environment.ts
@@ -0,0 +1,35 @@
+import { prisma } from '@/lib/prisma'
+import 'dotenv/config'
+import { execSync } from 'node:child_process'
+import { randomUUID } from 'node:crypto'
+import type { Environment } from 'vitest/environments'
+
+function generateDatabaseURL(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please provide a DATABASE_URL env variable.')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+  url.searchParams.set('schema', schema)
+  return url.toString()
+}
+
+export default <Environment>{
+  name: 'prisma',
+  transformMode: 'ssr',
+  async setup() {
+    const schema = randomUUID()
+    const databaseURL = generateDatabaseURL(schema)
+
+    process.env.DATABASE_URL = databaseURL
+    execSync('npx prisma db push', { stdio: 'inherit' })
+    return {
+      async teardown() {
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+        )
+        await prisma.$disconnect()
+      },
+    }
+  },
+}
