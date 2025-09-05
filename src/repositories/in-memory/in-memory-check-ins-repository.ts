e5290e97fb881764bcbe1948911031e4caba4202
Leonardo Freire Russi <freirerussi@gmail.com>
2025-09-05 13:18:18 -0300

    Caso de uso de histórico

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -40,4 +40,10 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
 
     return checkInOnSameDate
   }
+
+  async findManyByUserId(userId: string, page: number): Promise<CheckIn[]> {
+    return this.items
+      .filter((item) => item.user_id === userId)
+      .slice((page - 1) * 20, page * 20)
+  }
 }
