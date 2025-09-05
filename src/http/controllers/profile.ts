7f5ff2bff0fc70fbbadedf24268f262ad4f847c3
Leonardo Freire Russi <freirerussi@gmail.com>
2025-09-05 18:34:42 -0300

    Controller de perfil

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,7 +1,14 @@
+import { makeGetUserProfileUseCase } from '@/use-cases/factories/make-get-user-profile-use-case'
 import { FastifyReply, FastifyRequest } from 'fastify'
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
-  await request.jwtVerify()
+  const getUserProfile = makeGetUserProfileUseCase()
 
-  return reply.status(200).send()
+  const { user } = await getUserProfile.execute({ id: request.user.sub })
+  return reply.status(200).send({
+    user: {
+      ...user,
+      password_hash: undefined,
+    },
+  })
 }
